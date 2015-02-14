7403bec02af78de3fcbfac4fca9748c9b86096c3
Cheryl Lau <cheryllau.ck@hotmail.com>
2015-02-14 17:28:00 -0500

    created sign up form

--- deltarelations/urls.py
+++ deltarelations/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 from deltarelations import views
 
-urlpatterns = patterns('',
-  url(r'^$', views.index, name='index'),
+urlpatterns = patterns('', 
+  url(r'^sign_up', views.sign_up, name = 'sign_up'),
+  url(r'^$', views.index, name='index')
 )

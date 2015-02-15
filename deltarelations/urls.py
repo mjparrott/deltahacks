8a63344f35cbadf3e6f7ed27f897e71b9acc268f
Osmond Xie <funnydogo@hotmail.com>
2015-02-15 02:32:21 -0500

    added edit profile url

--- deltarelations/urls.py
+++ deltarelations/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
   url(r'^sign_up', views.sign_up, name = 'sign_up'),
   url(r'^log_in', views.log_in, name = 'log_in'),
   url(r'^view_matches', views.view_matches, name = 'view_matches'),
-  url(r'^$', views.index, name='index')
+  url(r'^$', views.index, name='index'),
+  url(r'^edit_profile', views.edit_profile, name = 'edit_profile')
 )
